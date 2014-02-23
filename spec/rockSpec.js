82e6aa2d3258ac69c985aa02b841548e7bf92bc4
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-02-23 22:21:14 +0000

    describing the Game -> test 1 + solution

--- spec/rockSpec.js
+++ spec/rockSpec.js
@@ -19,3 +19,15 @@ describe("Player", function(){
   });
 
 });
+
+describe("Game", function(){
+
+  it("is played by two players", function(){
+    player1 = new Player("Silent Bob");
+    player2 = new Player("Mini Me");
+    game = new Game(player1, player2);
+    expect(game.player1.name).toBe("Silent Bob");
+    expect(game.player2.name).toBe("Mini Me");
+  });
+
+});
--- src/rock.js
+++ src/rock.js
@@ -6,3 +6,7 @@ Player.prototype.picks = function(pick){
   return this.picks =  pick;
 };
 
+Game = function(player1, player2){
+  this.player1 = player1;
+  this.player2 = player2;
+};
